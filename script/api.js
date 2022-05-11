43bcc53bc2c17466f731b582a2c8480915d19f4b
Ony <onyrakoto27@gmail.com>
2022-05-11 14:41:09 +0300

    finish api integration

--- script/api.js
+++ script/api.js
@@ -1,88 +1,67 @@
 const quizTemplate = document.getElementById("quiz1-template");
-const answers = [];
 
-function getData() {
-	return new Promise((resolve, reject) => {
-		fetch("https://batman-api.herokuapp.com")
-			.then(response => response.json())
-			.then(data => resolve(data))
-	})
+async function getQuestion() {
+	const response = await fetch("https://plankton-app-mj9br.ondigitalocean.app/questions/all");
+	return response.json();
 }
 
-function checkIfTrueAnswer(idx) {
-	const current = answers[idx];
-	current.isTrue = current.answer === current.chosenAnswer;
-	console.log(answers);
-}
-
-function setChosenAnswer(answer, idx) {
-	answers[idx].chosenAnswer = answer;
-	checkIfTrueAnswer(idx);
-}
-
-function setRadioBtnClick() {
-	const allRadio = document.querySelectorAll("input[type=radio]");
-	allRadio.forEach(radio => {
-		radio.addEventListener("click", (event) => {
-			const label = event.target.nextElementSibling;
-			setChosenAnswer(+label.dataset.answerId, +label.dataset.quizId - 1);
-		})
-	})
-}
-
-const outputData = (quizs) => {
-	const quizOutput = document.importNode(quizTemplate.content, true);
-	const questionOutput = quizOutput.querySelector(".question");
-	const answer1 = quizOutput.querySelector(".choice-1 label");
-	const answer2 = quizOutput.querySelector(".choice-2 label");
-	const answer3 = quizOutput.querySelector(".choice-3 label");
-	const answer4 = quizOutput.querySelector(".choice-4 label");
+function importTemplate(templateName) {
+	const quizNode = document.importNode(quizTemplate.content, true);
+	const answerTemplate = quizNode.getElementById("answer-template");
+	const answerNode = document.importNode(answerTemplate.content, true);
+	let tempToExport = quizNode;
 	
-	const root = document.getElementById("root");
-	for (const quiz of quizs) {
-		questionOutput.textContent = quiz.question;
-		answer1.textContent = quiz.choice1;
-		answer1.dataset.answerId = "1";
-		answer2.textContent = quiz.choice2;
-		answer2.dataset.answerId = "2";
-		answer3.textContent = quiz.choice3;
-		answer3.dataset.answerId = "3";
-		answer4.textContent = quiz.choice4;
-		answer4.dataset.answerId = "4";
+	if (templateName === "answer") {
+		tempToExport = answerNode;
+	}
+	
+	return tempToExport;
+}
+
+function outputAnswer(quizEl, answerList) {
+	const parentElement = quizEl.querySelector(".choices");
+	const answerEl = importTemplate("answer");
+	
+	for (const answer of answerList) {
+		const id = `response_of_quiz_${parentElement.parentElement.id}--no_${answerList.indexOf(answer)}`;
+		const newAnswer = answerEl.cloneNode(true);
+		const radioBtn = newAnswer.querySelector("input");
+		const label = newAnswer.querySelector("label");
+		radioBtn.id = id;
+		label.setAttribute("for", id)
 		
-		quizOutput.querySelectorAll("label").forEach(label => {
-			label.dataset.quizId = quiz.id
-		});
-		
-		answer1.setAttribute("for", `question-${quiz.id}-1`);
-		answer1.previousElementSibling.id = `question-${quiz.id}-1`;
-		
-		answer2.setAttribute("for", `question-${quiz.id}-2`);
-		answer2.previousElementSibling.id = `question-${quiz.id}-2`;
-		
-		answer3.setAttribute("for", `question-${quiz.id}-3`);
-		answer3.previousElementSibling.id = `question-${quiz.id}-3`;
-		
-		answer4.setAttribute("for", `question-${quiz.id}-4`);
-		answer4.previousElementSibling.id = `question-${quiz.id}-4`;
-		
-		const answer = {
-			answer: quiz.answer,
-			chosenAnswer: null,
-			isTrue: null,
-		}
-		answers.push(answer);
-		const container = document.createElement("div");
-		container.append(quizOutput.cloneNode(true));
-		root.append(container);
+		label.textContent = answer.text;
+		parentElement.append(newAnswer);
 	}
 }
 
-const setData = async () => {
-	const data = await getData();
-	const questions = data.question;
-	outputData(questions);
-	setRadioBtnClick();
+function removeLoadingText(list) {
+	if (list.length > 0) {
+		document.getElementById("load").remove();
+	}
 }
 
-setData();
+async function outputQuestion() {
+	const outputRoot = document.getElementById("root");
+	const questionList = await getQuestion();
+	removeLoadingText(questionList);
+	let quizEl = importTemplate("quiz");
+	quizEl = quizEl.children[0];
+	
+	for (const question of questionList) {
+		const newQuiz = quizEl.cloneNode(true);
+		newQuiz.id = questionList.indexOf(question);
+		const questionOutputEl = newQuiz.querySelector(".question");
+		
+		questionOutputEl.textContent = question.question;
+		
+		outputAnswer(newQuiz, question.response)
+		outputRoot.append(newQuiz);
+	}
+}
+
+function output() {
+	outputQuestion();
+}
+
+output();
